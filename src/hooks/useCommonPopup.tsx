a804388189be4604839bddc8912e243c9360f141
jcmyeong88 <jcmyeong777@gmail.com>
2024-06-25 21:21:47 +0900

    feat: 공통 Popup Custom hook 추가

---
+++ src/hooks/useCommonPopup.tsx
@@ -0,0 +1,59 @@
+
+/**
+ * 공통 Popup Properties
+ */
+export interface CommonPopupProps {
+  title?: string              // Popup 제목
+  message: string             // Popup 메시지
+  confirmLabel?: string       // 확인 버튼 라벨
+  cancelLabel?: string        // 취소 버튼 라벨
+  onConfirm?: () => void      // 확인 버튼 이벤트
+  onCancel?: () => void       // 취소 버튼 이벤트
+}
+
+/**
+ * 공통 Popup custom hooks
+ */
+export const useCommonPopup = () => {
+
+  /**
+   * Alert Popup 표시
+   */
+  const showAlert = (props: CommonPopupProps) => {
+    const { title, message, onConfirm } = props
+    if (title) {
+      alert(`[${title}]\n\n${message}`)
+    } else {
+      alert(`${message}`)
+    }
+    
+    if (onConfirm !== undefined) {
+      onConfirm()
+    }
+  }
+
+  /**
+   * Confirm Popup 표시
+   */
+  const showConfirm = (props: CommonPopupProps) => {
+    const { title, message, onConfirm, onCancel } = props
+    let result: boolean
+    if (title) {
+      result = confirm(`[${title}]\n\n${message}`)
+    } else {
+      result = confirm(`${message}`)
+    }
+    
+    if (result) {
+      if (onConfirm !== undefined) {
+        onConfirm()
+      }
+    } else {
+      if (onCancel !== undefined) {
+        onCancel()
+      }
+    }
+  }
+
+  return { showAlert, showConfirm }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,23 +3,60 @@ import { useModal } from '@context/ModalContext'
 import Layout from '@components/Layout'
 import { Modal } from '../App';
 import PopupExample from '@components/Popup/PopupExample';
+import { useCommonPopup } from '@hooks/useCommonPopup';
+import styled from 'styled-components';
 
 const Home: React.FC = () => {
-  const { showModal } = useModal();
+  //const { showModal } = useModal();
 
-  const handleShowModal = () => {
-    showModal(<div>Welcome to the home page!</div>);
-  }
+  const { showAlert, showConfirm } = useCommonPopup()
+
+
+  // const handleShowModal = () => {
+  //   showModal(<div>Welcome to the home page!</div>);
+  // }
 
   const openPopup = () => Modal.open(PopupExample)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    const type = event.currentTarget.getAttribute('data-type')
+    if (type === 'alert') {
+      showAlert({
+        // title: 'Alert 팝업',
+        message: '이것은 Alert 팝업 예제 입니다.',
+      })
+    } else if (type === 'confirm') {
+      showConfirm({
+        // title: 'Confirm 팝업',
+        message: '현재 진행 상황을 저장하시겠습니까?',
+        onConfirm: () => { console.log('확인 클릭!') },
+        onCancel: () => { console.log('취소 클릭!') }
+      })
+    }
+  }
   
   return (
     <Layout>
       <h2>Home Page</h2>
       <p>Welcome to the home page!</p>
-      <button onClick={openPopup}>Show Modal</button>
+      <ChildNode>
+        <button onClick={openPopup}>Show Modal</button>
+      </ChildNode>
+      <ChildNode>
+        <button data-type="alert" onClick={handleClick}>Show Alert</button>
+      </ChildNode>
+      <ChildNode>
+        <button data-type="confirm" onClick={handleClick}>Show Confirm</button>
+      </ChildNode>
+      
     </Layout>
   );
-};
+}
+
+const ChildNode = styled.div`
+  display: flex;
+  margin-top: 20px;
+`
 
 export default Home;
